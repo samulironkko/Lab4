e96666b2ed4ee03cf162a3229e0d135746ac9cb9
Samuli <samuli.ronkko@gmail.com>
2019-02-04 00:19:02 +0200

    Puuttuu enää äänet.

--- app/src/main/java/com/example/lab4/RunProgramActivity.java
+++ app/src/main/java/com/example/lab4/RunProgramActivity.java
@@ -11,50 +11,41 @@ import java.util.ListIterator;
 
 public class RunProgramActivity extends AppCompatActivity {
 
-    int i;
     WorkoutPartBase current;
     TextView timeTextview;
+    TextView nameTextview;
+    ListIterator<WorkoutPartBase> iterator;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_run_program);
         ArrayList<WorkoutPartBase> workouts = (ArrayList<WorkoutPartBase>) getIntent().getSerializableExtra("WORKOUTS");
-        TextView nameTextview = (TextView)findViewById(R.id.workout_name);
+        nameTextview = (TextView)findViewById(R.id.workout_name);
         timeTextview = (TextView)findViewById(R.id.time_textview);
-        ListIterator<WorkoutPartBase> iterator = workouts.listIterator();
-        i = 0;
+        iterator = workouts.listIterator();
+        iterate();
+    }
+
+    private void iterate() {
+
+        if (iterator.hasNext() == false) {
+            finish();
+        }
 
         while (iterator.hasNext()) {
 
-            if (i == 0) {
-
-                current = iterator.next();
-                if (current instanceof WorkoutPart) {
-                    nameTextview.setText("Workout");
-                } else {
-                    nameTextview.setText("Pause");
-                }
-                startTimer();
-
-                /*
-                final String time = current.getTime();
-                long duration = Integer.parseInt(time) * 1000;
-
-                new CountDownTimer(duration, 1000) {
-                    @Override
-                    public void onTick(long l) {
-                        timeTextview.setText(String.valueOf(l / 1000));
-                    }
-
-                    @Override
-                    public void onFinish() {
-                        i = 0;
-                    }
-                }.start();
-                i = 1;
-                */
+            current = iterator.next();
+            if (current instanceof WorkoutPart) {
+                nameTextview.setText("Workout");
+            } else {
+                nameTextview.setText("Pause");
             }
+            startTimer();
+            iterator.remove();
+            break;
+
         }
     }
 
@@ -71,8 +62,7 @@ public class RunProgramActivity extends AppCompatActivity {
 
             @Override
             public void onFinish() {
-                i = 0;
-
+                iterate();
             }
         }.start();
     }
